675bc825006ba4f7d368737f47bfa87aff50726c
ch-sravan <110601810+ch-sravan@users.noreply.github.com>
2023-02-03 20:27:29 +0530

    Update Repeated Numbers.py

--- Repeated Numbers.py
+++ Repeated Numbers.py
@@ -15,3 +15,32 @@ for i in range(t):
         if(a[k]>1):
             print(b[k],end=" ")
     print()
+    
+    
+SOLUTION-2
+
+
+for _ in range(int(input())):
+    n=int(input())
+    l=list(map(int,input().strip().split()))
+    ans=0
+    for i in range(n):
+        ans^=l[i]
+    for i in range(1,n-1):
+        ans^=i
+    for i in range(32):
+        if((ans>>i)&1==1):
+            pos=i
+    a,b=0,0
+    for i in range(n):
+        if((l[i]>>pos)&1==1):
+            a^=l[i]
+        else:
+            b^=l[i]
+    for i in range(1,n-1):
+        if((i>>pos)&1==1):
+            a^=i
+        else:
+            b^=i
+    print(*sorted([a,b]))
+        
