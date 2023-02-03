a453adf1a9449c09cc0908d95f2aa55cb1dffffe
ch-sravan <110601810+ch-sravan@users.noreply.github.com>
2023-02-03 20:26:00 +0530

    Update Triple Trouble.py

--- Triple Trouble.py
+++ Triple Trouble.py
@@ -14,3 +14,21 @@ for i in range(t):
     for i in range(len(a)):
         if(a[i]==1):
             print(b[i])
+
+            
+SOLUTION-2
+
+
+for _ in range(int(input())):
+    n=int(input())
+    l=list(map(int,input().strip().split()))
+    ans=0
+    for b in range(32):
+        c=0
+        for i in range(n):
+            if ((l[i]>>b)&1)==1:
+                c+=1
+        if(c%3!=0):
+            ans+=1<<b
+    print(ans)
+            
